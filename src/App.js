f40143c9c5a1874fb383bcfa3bd8735b0a749dbd
mata0050 <mata0050@outlook.com>
2022-04-26 19:40:38 -0400

    Pages and page route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Routes, Route } from 'react-router';
 
 // CSS
 import './App.css';
 import Navbar from './components/Navbar';
 import GlobalStyles from './css/Global';
+import Home from './pages/Home';
+import Community from './pages/Community';
+import Features from './pages/Features';
+import Learn from './pages/Learn';
+import Pricing from './pages/Pricing';
 
 const theme = {
   colors: {
@@ -21,6 +27,13 @@ function App() {
       <Fragment>
         <GlobalStyles />
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/community' element={<Community />} />
+          <Route path='/features' element={<Features />} />
+          <Route path='/learn' element={<Learn />} />
+          <Route path='/pricing' element={<Pricing />} />
+        </Routes>
       </Fragment>
     </ThemeProvider>
   );
